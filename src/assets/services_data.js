ef9e10c76bb27fcfd60ba1a5104b6596d1e8f453
Dadben08 <Dadben08@gmail.comcleargit config --global user.email Dadben08@gmail.com>
2024-10-04 22:40:22 +0100

    new commit

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,33 +1,33 @@
 const Services_Data = [
     {
         s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Front-End Development",
+        s_desc:"I can create intuitive and responsive user interfaces, ensuring a seamless user experience across all devices. My skills in HTML, CSS, JavaScript, and modern frameworks(Bootstrap and Tailwind CSS.) helps me bring designs to life."
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Back-End Development",
+        s_desc:"I handle the server-side logic, using MongoDB with Mongoose for data storage and management, including CRUD operations and database schema, and APIs to ensure data integrity, performance, and security."
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Full-Stack Solutions",
+        s_desc:"I offer end-to-end solutions by integrating both front-end and back-end services into one cohesive application,  Developing complete e-commerce platforms with features like user authentication, product listings, shopping carts, and payment gateways."
     },
     {
         s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Development Tools and Deployment",
+        s_desc:"I also handle various development environments, testing, and deployment processes to ensure your applications are production-ready."
     },
     {
         s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Additional Skills",
+        s_desc:" Ensuring the website or app loads quickly and performs optimally, even under heavy user traffic."
     },
     {
         s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Computer Technician Services",
+        s_desc:"Hardware Repair and Maintenance, Data Backup and Recovery, Custom PC Building, Network Setup and Troubleshooting"
     },
 ]
  

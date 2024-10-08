6e42ed752fd557a008fa1d2d98ca01b64d9c45bf
Dadben08 <Dadben08@gmail.comcleargit config --global user.email Dadben08@gmail.com>
2024-10-08 20:53:38 +0100

    new commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
     <div id='home' className='hero'>
         <img src={profile_img} alt="" />
         <h1><span>I'm Dada Benjamin,</span> <br />A Fullstack developer based in Nigeria.</h1>
-        <p>I am a full stack Developer from Lagos, Nigeria and very eager to showcase what i have learn over six month of study </p>
+        <p>I am a full stack Developer from Lagos, Nigeria and very eager to showcase my skills. </p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className="anchor-link" offset={50} href= "#contact">Connect with me</AnchorLink></div>
             <div className="hero-resume"><a href={MyCV} download="My-CV.pdf">My resume</a></div>
